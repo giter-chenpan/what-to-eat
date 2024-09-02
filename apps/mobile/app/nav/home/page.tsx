c19e1f1b408d09ff72d4bcfa38ce0e2a6e1c7480
giter-chenpan <15310184859@163.com>
2024-09-02 17:46:21 +0800

    feat: request

--- apps/mobile/app/nav/home/page.tsx
+++ apps/mobile/app/nav/home/page.tsx
@@ -1,10 +1,21 @@
 "use client";
 
 import { Button } from "antd-mobile";
+import { useMutation } from "@tanstack/react-query";
+import Request from "@/common/request";
 
 export default function Home() {
+  const mutation = useMutation({
+    mutationFn: () => {
+      let file = document.getElementById("fileItem")?.files?.[0];
+      const formData = new FormData();
+      formData.append("file", file);
+      return Request.post("/api/category/import", formData);
+    },
+  });
+
   return (
-    <main className="p-4 h-full flex justify-center items-center">
+    <main className="p-4 h-full flex justify-center items-center flex-col">
       <Button
         color="primary"
         style={{
@@ -15,6 +26,9 @@ export default function Home() {
       >
         选一个
       </Button>
+
+      <input type="file" id="fileItem" />
+      <Button onClick={() => mutation.mutate()}>导入</Button>
     </main>
   );
 }
--- apps/mobile/next.config.mjs
+++ apps/mobile/next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    // output: 'export',
     transpilePackages: ['antd-mobile'],
+    rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://127.0.0.1:8088/api/:path*'
+            }
+        ]
+    }
 };
 
 export default nextConfig;
