670ffa4e5ad98f834a2caa7ef29abd240dc583d8
giter-chenpan <15310184859@163.com>
2024-12-09 16:31:18 +0800

    feat: ass dishes

--- apps/mobile/app/dishes/page.tsx
+++ apps/mobile/app/dishes/page.tsx
@@ -9,7 +9,7 @@ import {
   DatePickerRef,
   ImageUploader,
 } from "antd-mobile";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation } from "@tanstack/react-query";
 import Request from "@/common/request";
 
 export default function Dishes() {
@@ -17,12 +17,40 @@ export default function Dishes() {
   const { data: categories } = useQuery({
     queryKey: ["dishesCategoryList"],
     queryFn: () => Request.api.apiCategoryGetCategory(),
+    select: (res) =>
+      res.data.map((item: { name: any; id: any }) => ({
+        label: item.name,
+        value: item.id,
+      })),
   });
 
-  console.log(categories);
+  const uploadImg = useMutation({
+    mutationFn: async (file: File) => {
+      console.log(file);
+      const res = await Request.api.apiFileUploadFile({ file });
+      return res.data;
+    },
+  });
+
+  const handleSave = useMutation({
+    mutationFn: async () => {
+      const res = await form.validateFields();
+      const { categoryId, images, ...rest } = res;
+      await Request.api.apiDishesSaveDishes({
+        ...rest,
+        category_id: categoryId?.[0].toString(),
+        view_id: images?.[0].id.toString(),
+      });
+    },
+  });
+
   return (
     <div className="h-full dishes-page px-5 py-5 relative">
-      <Form form={form} style={{ "--border-bottom": "unset" }}>
+      <Form
+        form={form}
+        style={{ "--border-bottom": "unset" }}
+        layout="horizontal"
+      >
         <Form.Item
           name="name"
           label="菜谱名称"
@@ -34,29 +62,30 @@ export default function Dishes() {
 
         <Form.Item
           name="categoryId"
+          childElementPosition="right"
           label="分类"
           trigger="onConfirm"
           onClick={(_, datePickerRef: RefObject<DatePickerRef>) => {
             datePickerRef.current?.open();
           }}
         >
-          <Picker
-            columns={categories?.data || []}
-            fieldNames={{ label: "name", value: "id" }}
-          >
-            {(value) => (value ? value.join() : "Please select")}
+          <Picker columns={[categories]}>
+            {(value) => (value.length ? value[0]?.label : "请选择")}
           </Picker>
         </Form.Item>
 
-        <Form.Item name="desc" label="图片">
-          <ImageUploader upload={() => {}} />
+        <Form.Item name="images" label="图片">
+          <ImageUploader
+            upload={async (e) => await uploadImg.mutateAsync(e)}
+            maxCount={1}
+          />
         </Form.Item>
-        <Form.Item name="description" label="菜谱描述">
+        <Form.Item name="desc" label="菜谱描述">
           <Input placeholder="请输入菜谱描述" />
         </Form.Item>
       </Form>
       <div className="mt-5 absolute w-full px-5 left-0 bottom-6">
-        <Button color="primary" block>
+        <Button color="primary" block onClick={() => handleSave.mutate()}>
           保存
         </Button>
       </div>
