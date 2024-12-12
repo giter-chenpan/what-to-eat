5851c2f39dfe5e7aff6b17c46d13a1e1849f0d03
liuyan007 <517058183@qq.com>
2024-12-12 22:36:31 +0800

    feat: find api

--- apps/mobile/app/dishes/page.tsx
+++ apps/mobile/app/dishes/page.tsx
@@ -2,9 +2,18 @@
 
 import { Button } from 'antd-mobile'
 import { useRouter } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
+import Request from "@/common/request";
 
 export default function Dash() {
   const router = useRouter();
+
+  const data = useQuery({
+    queryKey: ["dishesList"],
+    queryFn: () => Request.api.apiDishesFindPage({}),
+  })
+  console.log(data)
+
   return <div className=' h-full' >
     <div className=' fixed bg-white p-2 w-full bottom-0'>
       <Button block color='primary' onClick={() => router.push("/dishes/add")}>新增</Button>
