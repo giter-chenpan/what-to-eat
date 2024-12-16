faee11c2fa3c05e4ff2d872701525f9331d88234
liuyan007 <517058183@qq.com>
2024-12-16 21:09:55 +0800

    feat: add show list

--- apps/mobile/app/dishes/page.tsx
+++ apps/mobile/app/dishes/page.tsx
@@ -1,21 +1,32 @@
 "use client"
 
-import { Button } from 'antd-mobile'
+import { Button, Collapse, Image } from 'antd-mobile'
 import { useRouter } from "next/navigation";
+import dayjs from 'dayjs'
 import { useQuery } from "@tanstack/react-query";
 import Request from "@/common/request";
 
 export default function Dash() {
   const router = useRouter();
 
-  const data = useQuery({
+  const { data } = useQuery({
     queryKey: ["dishesList"],
     queryFn: () => Request.api.apiDishesFindPage({}),
   })
   console.log(data)
 
-  return <div className=' h-full' >
-    <div className=' fixed bg-white p-2 w-full bottom-0'>
+  return <div className=' h-full p-3' >
+    <Collapse accordion>
+      {
+        data?.data?.list?.map(v => <Collapse.Panel key={v.id} title={v.name}>
+          <Image src={`/api/file/getimage?id=${v.id}`} alt='' lazy />
+          <div>{v.desc}</div>
+          <div>上传时间： {dayjs(v.create_time).format('YYYY-MM-DD HH:mm:ss')}</div>
+        </Collapse.Panel>)
+      }
+    </Collapse>
+    <div className=' fixed bg-white p-2 w-full bottom-0 left-0'>
+
       <Button block color='primary' onClick={() => router.push("/dishes/add")}>新增</Button>
     </div>
   </div>
